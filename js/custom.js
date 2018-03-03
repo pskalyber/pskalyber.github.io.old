4efc767c96906b7a3dff8a34e5e0f44645214b22
SKPARK <pskalyber@gmail.com>
2018-03-03 16:37:00 +0900

    홈페이지 접속시 #home만 보여주도록 custom.js 파일에 코드 추가.

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
+
+	$(".menu_template").each(function() {
+		if ($(this).attr("id") == "menu_home") {
+			$(this).show();
+		} else {
+			$(this).hide();
+		}
+	});
+
 	$(".nav-link").click(function() {
 		$(".nav-link").removeClass("active");
 		$(this).addClass("active");
-		
+
 		$('.navbar-collapse').collapse('hide');
 
 		menu_id = $(this).attr("target-menu");
