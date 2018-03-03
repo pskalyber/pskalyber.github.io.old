b35c86ee43b9ef191e71257b82be7e0ae549fb16
SKPARK <pskalyber@gmail.com>
2018-03-03 15:53:47 +0900

    메뉴 관련 자바스크립트 리팩토링
    - 불필요한 코드 제거
    -스크립트 파일 custom.js 파일로 통합하여 임포트
    - 메뉴 클릭시, 해당 메뉴 이동 후 메뉴 숨기기 (mobile)

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$(".nav-link").click(function() {
+		$(".nav-link").removeClass("active");
+		$(this).addClass("active");
+		
+		$('.navbar-collapse').collapse('hide');
+
+		menu_id = $(this).attr("target-menu");
+		$(".menu_template").each(function() {
+			if ($(this).attr("id") == menu_id) {
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+		});
+	});
+});
