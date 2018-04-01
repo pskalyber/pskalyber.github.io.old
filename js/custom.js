5571c148fd36f6accea5f068faf23e9efdb1031a
SKPARK <pskalyber@gmail.com>
2018-04-02 00:01:36 +0900

    컨텐츠 대대적 업데이트
    내용 업데이트
    새로고침 했을때 최근 페이지 그대로 열어주기 JS 업데이트

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,32 @@
 $(document).ready(function() {
 
-	$(".menu_template").each(function() {
-		if ($(this).attr("id") == "menu_home") {
-			$(this).show();
-		} else {
-			$(this).hide();
+	// active recently opened menu when refresh
+	var $menu_id = "menu_" + window.location.hash.substring(1)
+	$(".nav-link").each(function() {
+
+		$(this).removeClass("active");
+		if ($(this).attr("target-menu") == $menu_id) {
+			$(this).addClass("active");
 		}
+		
+		$('.navbar-collapse').collapse('hide');
+
+		menu_id = $(this).attr("target-menu");
+		$(".menu_template").each(function() {
+			if ($(this).attr("id") == $menu_id) {
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+		});
+
 	});
+	
+	$('.navbar-collapse').collapse('hide');
 
 	$(".nav-link").click(function() {
-		// only give an active class when the clicked menu is not CURRICULUM VITAE
+		// only give an active class when the clicked menu is not CURRICULUM
+		// VITAE
 		if ($(this).attr("target-menu") == "menu_cv") {
 			return;
 		} else {
